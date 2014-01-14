55f68c9099706e63e0a6b013d765ba38768c0c4e
tiborkovacs <tibor.kovacs@liferay.com>
2014-01-14 13:50:52 +0100

    SourceFormat on Asset Category Handlersˇ

--- docroot/WEB-INF/src/com/liferay/tool/datamanipulator/datatype/categories/AssetCategoryHandler.java
+++ docroot/WEB-INF/src/com/liferay/tool/datamanipulator/datatype/categories/AssetCategoryHandler.java
@@ -35,8 +35,8 @@ import com.liferay.tool.datamanipulator.service.DataManipulatorLocalServiceUtil;
  * @author Tibor Kovács
  *
  */
-public class AssetCategoryHandler extends AbstractEntryHandler implements
-		EntryHandlerModel {
+public class AssetCategoryHandler extends AbstractEntryHandler
+	implements EntryHandlerModel {
 
 	/**
 	 * @param count
@@ -59,6 +59,7 @@ public class AssetCategoryHandler extends AbstractEntryHandler implements
 	/* (non-Javadoc)
 	 * @see com.liferay.tool.datamanipulator.handler.entryhandler.model.EntryHandlerModel#getCreateEntryArgs(long, java.lang.String, com.liferay.tool.datamanipulator.requestprocessor.RequestProcessor)
 	 */
+
 	@Override
 	public EntryArgs getCreateEntryArgs(
 			long parentId, String postString, RequestProcessor requestProcessor)
@@ -97,6 +98,7 @@ public class AssetCategoryHandler extends AbstractEntryHandler implements
 	/* (non-Javadoc)
 	 * @see com.liferay.tool.datamanipulator.handler.entryhandler.model.EntryHandlerModel#getDataManipulatorFromObject(java.lang.Object)
 	 */
+
 	@Override
 	public DataManipulator getDataManipulatorFromObject(Object createdEntry)
 		throws PortalException, SystemException {
@@ -110,13 +112,14 @@ public class AssetCategoryHandler extends AbstractEntryHandler implements
 	/* (non-Javadoc)
 	 * @see com.liferay.tool.datamanipulator.handler.entryhandler.model.EntryHandlerModel#getUpdateEntryArgs(long, java.lang.String, com.liferay.tool.datamanipulator.requestprocessor.RequestProcessor)
 	 */
+
 	@Override
 	public EntryArgs getUpdateEntryArgs(
 			long entryId, String postString, RequestProcessor requestProcessor)
 		throws PortalException, SystemException {
 
-		AssetCategory category =
-			AssetCategoryLocalServiceUtil.getCategory(entryId);
+		AssetCategory category = AssetCategoryLocalServiceUtil.getCategory(
+			entryId);
 
 		String title = EntryUtil.getEditString(
 			category.getTitle(LocaleUtil.getDefault()), postString);
