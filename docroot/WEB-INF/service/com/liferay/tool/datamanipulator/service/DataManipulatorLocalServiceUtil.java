1a056f7c0a86a22771e427fa6a47e30c9920b975
tiborkovacs <tibor.kovacs@liferay.com>
2014-02-11 22:31:23 +0100

    DMLSI remove the ClassLoader and built-service

--- docroot/WEB-INF/service/com/liferay/tool/datamanipulator/service/DataManipulatorLocalServiceUtil.java
+++ docroot/WEB-INF/service/com/liferay/tool/datamanipulator/service/DataManipulatorLocalServiceUtil.java
@@ -256,6 +256,11 @@ public class DataManipulatorLocalServiceUtil {
 		getService().setBeanIdentifier(beanIdentifier);
 	}
 
+	/**
+	* NOTE FOR DEVELOPERS:
+	*
+	* Never reference this interface directly. Always use {@link com.liferay.tool.datamanipulator.service.DataManipulatorLocalServiceUtil} to access the data manipulator local service.
+	*/
 	public static com.liferay.tool.datamanipulator.model.DataManipulator addDataManipulator(
 		long groupId, java.lang.String className, long classPK)
 		throws com.liferay.portal.kernel.exception.SystemException {
